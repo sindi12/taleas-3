d68a3168c69e5d1737d33d79b99d17f044647d07
sindi12 <45913632+sindi12@users.noreply.github.com>
2019-08-09 03:20:03 +0200

    Update TaleasApplication.java

--- src/main/java/com/taleas/taleas/TaleasApplication.java
+++ src/main/java/com/taleas/taleas/TaleasApplication.java
@@ -2,12 +2,17 @@ package com.taleas.taleas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@RefreshScope
+//@EnableEurekaClient
 public class TaleasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaleasApplication.class, args);
 	}
-
 }
